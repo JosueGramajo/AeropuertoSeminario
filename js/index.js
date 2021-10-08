a2f4226046fcf978ed5ee7e5acfefe1cfa00c70f
Leonardo Sespedes <jgramajo@facturas.gt>
2021-10-07 20:52:30 -0600

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$("#btnContinueSearch").click(function(){
+		window.location.href = "seatSelector2.html";
+	});
+
+	$("#btnRegister").click(function(){
+		window.location.href = "register.html";
+	});
+
+	$("#btnLogin").click(function(){
+		window.location.href = "login.html";
+	});
+
+	$("#btnContinueToPayment").click(function(){
+		window.location.href = "payment.html";
+	});
+});
